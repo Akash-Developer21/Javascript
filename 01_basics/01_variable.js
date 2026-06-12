067e6f14903a1ba85fa69ace2057f1138199f380
Akash-Developer21 <shashikant854503@gmail.com>
2026-06-12 10:21:09 +0000

    learned about variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,26 @@
+const  name = "Akash";
+var city_name = "mumbai"
+let village_name = "darbhanga"
+/*
+ var ka use isliye nhi karte q ki ham jitne var use karange vo vakues ko change karta rahega infact {} iske aandar bhi var values ko cahnge karta hai par let nhi kaarta jjisse data secure rehta hai 
+*/
+//eg for var 
+var city_name = "panjab"
+var city_name = "up"
+
+//eg for let
+//let village_name = "gorakhpur"
+//in output it will give SyntaxError: Identifier 'village_name' has already been declared
+
+
+
+//name = "Amulya"// const mai kuch bhi change nhi kar sakte ek baar jo value assign kar di fir change nhi hoga 
+//city_name = "karnataka"
+//village_name = "delhi" 
+
+console.log(city_name)
+console.log(village_name)
+console.log(name)
+
+//it caan be printed in table formate too 
+console.table([city_name,village_name,name])
