db501b9831cc05971f035e36d9d78133f7acc03c
Akash-Developer21 <shashikant854503@gmail.com>
2026-06-13 06:55:34 +0000

    learning datatypes

---
+++ 01_basics/02_Datatype.js
@@ -0,0 +1,27 @@
+"usse strict" // use newer version of javascrpit 
+
+//alert(4+4) // we are using in node not in browser 
+console.log( 4
+    +
+    4
+); // this will still give you output but code readbility decreases 
+
+let name = "Amul doodh"
+let number = 5 
+let isloggedin = "true"
+let state;
+
+// integer => 2 the power of 53
+// bigint 
+// boolean => "true or false"
+// string => ""
+// undefined =>
+// null => standalone value
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof undefine) // undefine 
+console.log(typeof null)     // object //JavaScript ke first version me values ko binary format me store kiya jata tha. Object values ke liye type tag 000 use hota tha.null ki binary representation bhi accidentally 000 aa gayi, isliye
