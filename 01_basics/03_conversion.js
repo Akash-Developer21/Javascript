12c8d6008092300e38ce0d921eac7288e464e0e4
Akash-Developer21 <shashikant854503@gmail.com>
2026-06-13 07:31:16 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,49 @@
+let name = "21"
+console.log(name);
+
+//conversion of this string into number 
+
+let numbername = Number(name)
+console.log(numbername)
+console.log(typeof numbername)
+//output
+// 21
+// 21
+// number 
+
+let value = "21aa"
+console.log(value);
+
+//conversion of this string into number 
+
+let valueinnumber = Number(value)
+console.log(valueinnumber)
+console.log(typeof valueinnumber)
+//output
+//21aa
+//NaN
+//number
+
+//similarly we change string into number and vise versa 
+
+
+
+// now we check for boolean 
+
+let isloggedin = true
+console.log(isloggedin)
+//convert into number 
+let isloggedininnumber = Number(isloggedin)
+console.log(isloggedininnumber)
+console.log(typeof isloggedininnumber)
+
+//output
+//true
+//NaN
+//number
+//similarly we can convert number in boolean 
+
+// "" => false
+// "Akash" => true
+
+
